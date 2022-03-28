3e061204486e60a3bf6491c8dec3d82f59eeb719
nevenxr <nevenxr@gmail.com>
2022-03-28 17:18:44 +0600

    feat(app): add home page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>LinkR - Keep it short :)</title>
+      </Head>
+      <h1 className="text-center text-black">Hello World</h1>
+    </>
+  );
+};
