d89a794747ffd6997ddf3c9bb5efcd9946813b5f
nevenxr <nevenxr@gmail.com>
2022-03-28 17:01:28 +0600

    feat(backend): Main file for the backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const express = require("express");
+
+app.prepare().then(() => {
+    const app = express();
+    
+    require("./utils/middleware")(app);
+    require("./utils/routes")(app);
+    app.get("*", async (req, res) => {
+        return await handle(req, res);
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Link shortener is started")
+    });
+});
